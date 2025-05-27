f58f147605ef44d4424a9b848d413cd533504bb2
H4ze123 <ilij.stoj@gmail.com>
2025-05-27 22:43:06 +0200

    Update

---
+++ HyprlandDots/Config/rofi/mode.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+##
+## Original Author : Aditya Shakya (adi1090x)
+## Original Github : @adi1090x
+## Adapted by : @GeodeArc
+##
+
+# Current Theme
+dir="$HOME/.config/rofi/mode/"
+theme='main'
+
+# Options
+lmode='☀️'
+dmode='🌙'
+gmode='🎮️'
+
+# Rofi CMD
+rofi_cmd() {
+	rofi -dmenu \
+		-p " $USER" \
+		-mesg "Light | Dark | Minimal" \
+		-theme ${dir}/${theme}.rasi
+}
+
+# Pass variables to rofi dmenu
+run_rofi() {
+	echo -e "$lmode\n$dmode\n$gmode" | rofi_cmd
+}
+
+# Actions
+chosen="$(run_rofi)"
+case ${chosen} in
+    $lmode)
+    	$HOME/Dots/Scripts/Themes/light.sh
+    	notify-send -i view-reveal-symbolic "Light Mode Active"
+        ;;
+    $dmode)
+    	$HOME/Dots/Scripts/Themes/dark.sh
+    	notify-send -i view-reveal-symbolic "Dark Mode Active"
+        ;;
+    $gmode)
+		$HOME/Dots/Scripts/Themes/game.sh
+		notify-send -i applications-games-symbolic "Game Mode Active"
+        ;;
+esac
